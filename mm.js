a620b84ca54c1eea5f79810ff92ebbeeefa68c6b
nko <solowingrazgriz@outlook.com>
2023-01-25 13:14:04 +0900

    Initial psuedocode & planning, add initial word arrays

---
+++ mm.js
@@ -0,0 +1,25 @@
+//The Mixed Messages project will function as a random message generator.
+/* We'll try this by studying sentence structure.
+
+English sentence structure:
+Subject + verb
+
+Components:
+Verbs
+Nouns
+    Singular
+    Plural
+Article
+    Singular
+    Plural
+Adjectives
+
+Build arrays for each of these?
+Program would pick at random from each array
+
+*/
+
+const verbs = [];
+const nouns = ["fox", "robot", "goblin"];
+const articles = ["a", "the", "an"];
+const adjective = ["sleepy", "fiery", "calm", "golden"];
