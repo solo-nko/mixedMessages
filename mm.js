66d10d216a9f7e0db3dd0864275664baa8322db7
nko <solowingrazgriz@outlook.com>
2023-01-27 14:58:36 +0900

    Add first part of constructSentence()

--- mm.js
+++ mm.js
@@ -20,27 +20,43 @@ Program would pick at random from each array
 */
 
 const verbs = ["eats", "strikes", "wipes"];
-const nouns = ["fox", "robot", "goblin", "road"];
-const articles = ["the", "a","an"]; //choose based on vowel/consonant;
+const nouns = ["fox", "robot", "goblin", "road", "ape", "eye", "knife", "ogre", "angler"];
+const articles = ["the", "a", "an"]; //choose based on vowel/consonant;
 const adjective = ["sleepy", "fiery", "calm", "golden"];
-
-let randomSentence = [];
+const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "b", "w", "x", "w,", "y", "z"]
+const vowels = ["a", "e", "i", "o", "u"];
 
 function randomSelection(inputArray)
 {
-	let chosenIndex = Math.floor(Math.random() * inputArray.length-1);
+	let chosenIndex = Math.floor(Math.random() * inputArray.length);
 	return inputArray[chosenIndex];
 }
 
 function constructSentence()
 {
-	randomSentence.push(randomSelection(articles));
-		if (randomSentence[0] === "a")
+	let randomSentenceArray = [];
+	randomSentenceArray.push(randomSelection(articles));
+	let testWord = randomSelection(nouns);//pull a noun from the deck
+	if (randomSentenceArray[0] === "a")
+	{
+		while (vowels.includes(testWord[0]))//if the noun pulled starts with aiueo, draw again.
 		{
-			
+			testWord = randomSelection(nouns);
 		}
-		else if (randomSentence[0] === "an")
+		randomSentenceArray.push(testWord);
+	}
+	else if (randomSentenceArray[0] === "an")
+	{
+		while (consonants.includes(testWord[0]))//if the noun pulled does NOT start with aiueo, draw again.  Currently broken, because I think 
 		{
+			testWord = randomSelection(nouns);
+		}
+		randomSentenceArray.push(testWord);
+	} else
+	{
+		randomSentenceArray.push(testWord);
+	}
+	return randomSentenceArray.join(" ");
+}
 
-		}
-}    
+console.log(constructSentence());
