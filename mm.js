204aa427a73ab6e3f1b8048a5be3db027935f82a
nko <solowingrazgriz@outlook.com>
2023-02-06 16:33:14 +0900

    Improve word choice logic, expand word arrays

--- mm.js
+++ mm.js
@@ -17,11 +17,13 @@ Adjectives
 
 */
 
-const verbs = ["eats", "strikes", "wipes", "pushes", "pulls", "drops", "throws"];
-const nouns = ["fox", "robot", "goblin", "road", "ape", "eye", "knife", "ogre", "angler", "ostrich", "imp"];
+//Next task: Presently the program chooses words based on the article, when in reality it's the reverse: we choose the appropriate article based on the words we want to use.  Alter the program to follow this logic.
+
+const verbs = ["eats", "strikes", "wipes", "pushes", "pulls", "drops", "throws", "sends", "spins"];
+const nouns = ["fox", "robot", "goblin", "road", "ape", "eye", "knife", "ogre", "angler", "ostrich", "imp", "king"];
 const articles = ["the", "a", "an"]; //choose based on vowel/consonant;
-const adjectives = ["sleepy", "fiery", "calm", "golden"];
-const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "b", "w", "x", "w,", "y", "z"]
+const adjectives = ["sleepy", "fiery", "calm", "golden", "red", "wily", "heavy", "quick", "easy", "old", "shy", "aloof"];
+const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 const vowels = ["a", "e", "i", "o", "u"];
 
 function randomSelection(inputArray)
@@ -35,64 +37,64 @@ function constructSentence()
 	let randomSentenceArray = [];
 	randomSentenceArray.push(randomSelection(articles)); //draw random article from the deck and add it to the sentence
 	let testAdjective = randomSelection(adjectives); //draw random adjective from the deck
-	randomSentenceArray.push(testAdjective); //add drawn adjective to the sentence
-	let testWord = randomSelection(nouns); //draw a random noun from the deck
+	let testNoun = randomSelection(nouns); //draw a random noun from the deck
 
 	if (randomSentenceArray[0] === "a")
 	{
-		while (vowels.includes(testWord[0])) //if the noun pulled starts with aiueo, draw again.
+		while (vowels.includes(testAdjective[0])) //if the adjective pulled starts with a vowel, draw again.
 		{
-			testWord = randomSelection(nouns);
+			testAdjective = randomSelection(adjectives);
 		}
-		randomSentenceArray.push(testWord);
+		randomSentenceArray.push(testAdjective);
 	}
 	else if (randomSentenceArray[0] === "an")
 	{
-		while (consonants.includes(testWord[0])) //if the noun pulled does NOT start with aiueo, draw again.  Currently broken, because I think 
+		while (consonants.includes(testAdjective[0])) //if the adjective pulled starts with a consonant, draw again. 
 		{
-			testWord = randomSelection(nouns);
+			testAdjective = randomSelection(adjectives);
 		}
-		randomSentenceArray.push(testWord);
+		randomSentenceArray.push(testAdjective);
 	} else
 	{
-		randomSentenceArray.push(testWord);
+		randomSentenceArray.push(testAdjective);
 	}
+	randomSentenceArray.push(testNoun);
 
 	randomSentenceArray.push(randomSelection(verbs)); //draw random verb from the deck and add it to the sentence
 	randomSentenceArray.push(randomSelection(articles)); //draw random article from the deck and add it to the sentence
 	
 	testAdjective = randomSelection(adjectives); //draw random adjective from the deck
-	while (randomSentenceArray.includes(adjectives)) //check if the adjective drawn is already present in the sentence. if it is, draw a new one
+	while (randomSentenceArray.includes(testAdjective)) //check if the adjective drawn is already present in the sentence. if it is, draw a new one
 	{
-		testWord = randomSelection(adjectives);
-	}
-	randomSentenceArray.push(testAdjective); //add adjective to the sentence
-
-	testWord = randomSelection(nouns); //draw random noun from the deck
-	while (randomSentenceArray.includes(testWord)) //check if the noun drawn is already present in the sentence.  if it is, draw a new one
-	{
-		testWord = randomSelection(nouns);
+		testAdjective = randomSelection(adjectives);
 	}
 
 	if (randomSentenceArray[4] === "a")
 	{
-		while (vowels.includes(testWord[0])) //if the noun pulled starts with aiueo, draw again.
+		while (vowels.includes(testAdjective[0])) //if the adjective pulled starts with a vowel, draw again.
 		{
-			testWord = randomSelection(nouns);
+			testAdjective = randomSelection(adjectives);
 		}
-		randomSentenceArray.push(testWord);
+		randomSentenceArray.push(testAdjective);
 	}
 	else if (randomSentenceArray[4] === "an")
 	{
-		while (consonants.includes(testWord[0])) //if the noun pulled does NOT start with aiueo, draw again.  Currently broken, because I think 
+		while (consonants.includes(testAdjective[0])) //if the adjective pulled starts with a consonant, draw again.
 		{
-			testWord = randomSelection(nouns);
+			testAdjective = randomSelection(adjectives);
 		}
-		randomSentenceArray.push(testWord);
+		randomSentenceArray.push(testAdjective);
 	} else
 	{
-		randomSentenceArray.push(testWord);
+		randomSentenceArray.push(testAdjective);
 	}
+	
+	testNoun = randomSelection(nouns); //draw random noun from the deck
+	while (randomSentenceArray.includes(testNoun)) //check if the noun drawn is already present in the sentence.  if it is, draw a new one
+	{
+		testNoun = randomSelection(nouns);
+	}
+	randomSentenceArray.push(testNoun);
 	
 	let newSentence = randomSentenceArray.join(" "); //create a string from the array
 	newSentence = newSentence.charAt(0).toUpperCase() + newSentence.slice(1) + "."; //capitalize the sentence and add a period
