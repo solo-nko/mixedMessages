c3168a48cfe4955009396b89835e96a4faaab521
nko <solowingrazgriz@outlook.com>
2023-02-08 10:58:34 +0900

    Rewrite word selection logic

--- mm.js
+++ mm.js
@@ -35,65 +35,61 @@ function randomSelection(inputArray)
 function constructSentence()
 {
 	let randomSentenceArray = [];
-	randomSentenceArray.push(randomSelection(articles)); //draw random article from the deck and add it to the sentence
+	let testArticle = randomSelection(articles);
 	let testAdjective = randomSelection(adjectives); //draw random adjective from the deck
 	let testNoun = randomSelection(nouns); //draw a random noun from the deck
 
-	if (randomSentenceArray[0] === "a")
+	if (vowels.includes(testAdjective[0])) //if the adjective drawn starts with a vowel...
 	{
-		while (vowels.includes(testAdjective[0])) //if the adjective pulled starts with a vowel, draw again.
+		while (testArticle === "a") //..and if the article drawn is "a", draw a new article
 		{
-			testAdjective = randomSelection(adjectives);
+			testArticle = randomSelection(articles);
 		}
-		randomSentenceArray.push(testAdjective);
 	}
-	else if (randomSentenceArray[0] === "an")
+	else if (consonants.includes(testAdjective[0]))
 	{
-		while (consonants.includes(testAdjective[0])) //if the adjective pulled starts with a consonant, draw again. 
+		while (testArticle === "an") //if the adjective pulled starts with a consonant, draw again. 
 		{
-			testAdjective = randomSelection(adjectives);
+			testArticle = randomSelection(articles);
 		}
-		randomSentenceArray.push(testAdjective);
-	} else
-	{
-		randomSentenceArray.push(testAdjective);
 	}
+	randomSentenceArray.push(testArticle);
+	randomSentenceArray.push(testAdjective);
+
 	randomSentenceArray.push(testNoun);
 
 	randomSentenceArray.push(randomSelection(verbs)); //draw random verb from the deck and add it to the sentence
-	randomSentenceArray.push(randomSelection(articles)); //draw random article from the deck and add it to the sentence
 	
+	testArticle = randomSelection(articles);
 	testAdjective = randomSelection(adjectives); //draw random adjective from the deck
+	testNoun = randomSelection(nouns);
+	
 	while (randomSentenceArray.includes(testAdjective)) //check if the adjective drawn is already present in the sentence. if it is, draw a new one
 	{
 		testAdjective = randomSelection(adjectives);
 	}
-
-	if (randomSentenceArray[4] === "a")
-	{
-		while (vowels.includes(testAdjective[0])) //if the adjective pulled starts with a vowel, draw again.
-		{
-			testAdjective = randomSelection(adjectives);
-		}
-		randomSentenceArray.push(testAdjective);
-	}
-	else if (randomSentenceArray[4] === "an")
-	{
-		while (consonants.includes(testAdjective[0])) //if the adjective pulled starts with a consonant, draw again.
-		{
-			testAdjective = randomSelection(adjectives);
-		}
-		randomSentenceArray.push(testAdjective);
-	} else
-	{
-		randomSentenceArray.push(testAdjective);
-	}
-	
-	testNoun = randomSelection(nouns); //draw random noun from the deck
-	while (randomSentenceArray.includes(testNoun)) //check if the noun drawn is already present in the sentence.  if it is, draw a new one
+	while (randomSentenceArray.includes(testNoun))
 	{
 		testNoun = randomSelection(nouns);
 	}
+
+	if (vowels.includes(testAdjective[0])) //if the adjective drawn starts with a vowel...
+	{
+		while (testArticle === "a") //..and if the article drawn is "a", draw a new article
+		{
+			testArticle = randomSelection(articles);
+		}
+	}
+	else if (consonants.includes(testAdjective[0]))
+	{
+		while (testArticle === "an") //if the adjective pulled starts with a consonant, draw again. 
+		{
+			testArticle = randomSelection(articles);
+		}
+	}
+
+	randomSentenceArray.push(testArticle);
+	randomSentenceArray.push(testAdjective);
 	randomSentenceArray.push(testNoun);
 	
 	let newSentence = randomSentenceArray.join(" "); //create a string from the array
