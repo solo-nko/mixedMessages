4965fdc907c2f727b7fd479069c3717a262b3986
nko <solowingrazgriz@outlook.com>
2023-02-08 11:25:00 +0900

    Slightly shave down program, add words

--- mm.js
+++ mm.js
@@ -17,12 +17,10 @@ Adjectives
 
 */
 
-//Next task: Presently the program chooses words based on the article, when in reality it's the reverse: we choose the appropriate article based on the words we want to use.  Alter the program to follow this logic.
-
-const verbs = ["eats", "strikes", "wipes", "pushes", "pulls", "drops", "throws", "sends", "spins"];
-const nouns = ["fox", "robot", "goblin", "road", "ape", "eye", "knife", "ogre", "angler", "ostrich", "imp", "king"];
+const verbs = ["eats", "strikes", "wipes", "pushes", "pulls", "drops", "throws", "sends", "spins", "limits", "studies", "uses", "observes", "underestimates"];
+const nouns = ["fox", "robot", "goblin", "road", "ape", "eye", "knife", "ogre", "angler", "ostrich", "imp", "king", "queen", "prince", "princess", "frog"];
 const articles = ["the", "a", "an"]; //choose based on vowel/consonant;
-const adjectives = ["sleepy", "fiery", "calm", "golden", "red", "wily", "heavy", "quick", "easy", "old", "shy", "aloof"];
+const adjectives = ["sleepy", "fiery", "calm", "golden", "red", "wily", "heavy", "quick", "easy", "old", "shy", "aloof", "amused", "energetic", "angelic", "eccentric", "austere"];
 const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 const vowels = ["a", "e", "i", "o", "u"];
 
@@ -35,54 +33,55 @@ function randomSelection(inputArray)
 function constructSentence()
 {
 	let randomSentenceArray = [];
-	let testArticle = randomSelection(articles);
-	let testAdjective = randomSelection(adjectives); //draw random adjective from the deck
-	let testNoun = randomSelection(nouns); //draw a random noun from the deck
+	let testArticle = randomSelection(articles); //draw a random article from the array
+	let testAdjective = randomSelection(adjectives); //draw random adjective from the array
+	let testNoun = randomSelection(nouns); //draw a random noun from the array
 
 	if (vowels.includes(testAdjective[0])) //if the adjective drawn starts with a vowel...
 	{
-		while (testArticle === "a") //..and if the article drawn is "a", draw a new article
+		while (testArticle === "a") //..and if the article drawn is "a", draw a new article, because we do not use "a" with vowels
 		{
 			testArticle = randomSelection(articles);
 		}
 	}
-	else if (consonants.includes(testAdjective[0]))
+	else if (consonants.includes(testAdjective[0])) //or, if the adjective drawn starts with a consonant...
 	{
-		while (testArticle === "an") //if the adjective pulled starts with a consonant, draw again. 
+		while (testArticle === "an") //...and if the article drawn is "an", draw a new article, because we do not use "an" with consonants.
 		{
 			testArticle = randomSelection(articles);
 		}
 	}
+
+	//after getting the appropriate words sorted, we push them to the array in order
 	randomSentenceArray.push(testArticle);
 	randomSentenceArray.push(testAdjective);
-
 	randomSentenceArray.push(testNoun);
 
+	//second part of the sentence
 	randomSentenceArray.push(randomSelection(verbs)); //draw random verb from the deck and add it to the sentence
 	
 	testArticle = randomSelection(articles);
-	testAdjective = randomSelection(adjectives); //draw random adjective from the deck
-	testNoun = randomSelection(nouns);
 	
-	while (randomSentenceArray.includes(testAdjective)) //check if the adjective drawn is already present in the sentence. if it is, draw a new one
+	while (randomSentenceArray.includes(testAdjective)) //replace the currently drawn adjective with a new one
 	{
 		testAdjective = randomSelection(adjectives);
 	}
-	while (randomSentenceArray.includes(testNoun))
+	while (randomSentenceArray.includes(testNoun)) //replace the currently drawn noun with a new one
 	{
 		testNoun = randomSelection(nouns);
 	}
 
-	if (vowels.includes(testAdjective[0])) //if the adjective drawn starts with a vowel...
+	//repeat tests from the first part to ensure grammatical accuracy
+	if (vowels.includes(testAdjective[0])) 
 	{
-		while (testArticle === "a") //..and if the article drawn is "a", draw a new article
+		while (testArticle === "a")
 		{
 			testArticle = randomSelection(articles);
 		}
 	}
 	else if (consonants.includes(testAdjective[0]))
 	{
-		while (testArticle === "an") //if the adjective pulled starts with a consonant, draw again. 
+		while (testArticle === "an")
 		{
 			testArticle = randomSelection(articles);
 		}
